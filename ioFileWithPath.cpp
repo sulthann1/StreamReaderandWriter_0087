5c5674d2c70dde2e05a750c529bbcafdffbfac06
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-10 15:34:55 +0700

    membuka file dan melakukan perulangan setiap baris dalam mode membaca

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -12,5 +12,37 @@ int main(){
 
     //membuka file dalam mode menulis.
     ofstream outfile;
-    
+    //menunjuk ke sebuah file
+    outfile.open (NamaFile+ ".txt", ios::out);
+    cout << ">= Menulis file, \'q\' untuk keluar" <<endl;
+    // unlimited loop untuk menulis
+    while(true){
+        cout << "-";
+        //mendapatkan setiap karakter dalam satu baris
+        getline (cin,baris); 
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris ==  "q" ) break;
+        //menulis dan memasukan nilai dari 'baris' kedlam file
+        outfile <<baris <<endl;
+
+        //selesai dalam menulis sekarang tutup file nya
+        outfile.close();
+        //membuka file dalam metode membaca
+        ifstream infile;
+        //menunjuk ke sebuah file
+        infile.open(NamaFile, ios :: in);
+        cout << endl << ">= Membuka dan membaca file" <<endl;
+        if (infile.is_open())
+        {
+            //melakukan perulangan setiap baris
+            while (getline (infile, baris))
+            {
+                //dan tampilkan disini
+                cout <<baris <<endl;
+            }
+        }
+
+
+    }
+ 
 }
