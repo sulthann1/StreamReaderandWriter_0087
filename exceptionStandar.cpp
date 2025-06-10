d9b70d963ea8713f028f6f88084739de68267dc8
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-10 15:07:41 +0700

    int main

--- exceptionStandar.cpp
+++ exceptionStandar.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
 #include <exception>
 
-//untuk
+//untuk obyek exception yang akan digunakan
+#include <array>
+
+//untuk array yang akan digunakan
+using namespace std;
+
+int main()
+{
+    cout << "awal program" <<endl; //penanda 1:......
+    try {
+        array <int, 3> data= {10,20,30};
+        //pesan array integer 3 elemen
+    }
+}
