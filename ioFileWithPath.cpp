47fdc8b53668d2d21c129e3f6ee8c0bb3e41532a
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-10 15:23:26 +0700

    int main iofilewithpath

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -3,3 +3,14 @@
 #include <string>
 using namespace std;
 
+int main(){
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan nama file :";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis.
+    ofstream outfile;
+    
+}
