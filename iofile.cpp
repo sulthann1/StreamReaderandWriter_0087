c7c1a343137fe85409ea4f7de028b4b7e3d38521
sulthann1 <sulthanaawaliya155@gmail.com>
2025-06-10 14:49:11 +0700

    loop berhenti jika menenukan q

--- iofile.cpp
+++ iofile.cpp
@@ -3,21 +3,28 @@
 #include <string>
 using namespace std;
 
-int main(){
+int main()
+{
     string baris;
 
-    //menambahkan file ke dalam mode menulis
+    // menambahkan file ke dalam mode menulis
     ofstream outfile;
 
-     //menunjuk ke sebuah nama file
-    outfile.open ("contohfile.txt");
+    // menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
 
-    cout <<">= Menulis file. \'q\' untuk keluar " <<endl;
+    cout << ">= Menulis file. \'q\' untuk keluar " << endl;
 
-    //unlimited loop untuk menulis
-    while(true) {
+    // unlimited loop untuk menulis
+    while (true)
+    {
         cout << "_";
-        //mendapatkan setiap karakter dalam satu baris
-        getline
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+
+        //loop akan berhenti jika memasukan karakter q
+        if (baris =="q") break;
+
+       
     }
 };
